3c6806cdd85e81dd15ad12a061e8bb963fdd294c
DemarcoBryant <158539299+DemarcoBryant@users.noreply.github.com>
2024-02-12 13:13:39 -0500

    monthly balance

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args) {
+        double creditCardBalance = 3000;
+        double INTEREST_RATE = .17;
+        double interest = creditCardBalance * INTEREST_RATE;
+        double monthOne = interest * 1;
+        double monthTwo = interest * 2;
+        System.out.println("Your credit card balance is " + creditCardBalance + " and your interest rate is " + INTEREST_RATE);
+        System.out.println("Your interest due after one month is " + monthOne);
+        System.out.println("Your interest due after two months is " + monthTwo);
+    }
+}
